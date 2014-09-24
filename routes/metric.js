15e57c5774705d9cf8b5e8b27bffe43087f05ca4
jwd2a <jdavis1002@gmail.com>
2014-09-23 22:34:12 -0400

    cleanup

--- routes/metric.js
+++ routes/metric.js
@@ -3,25 +3,22 @@ var router = express.Router();
 var Metric = require("../models/metric").model;
 var _ = require("underscore");
 
-/* GET home page. */
 router.get('/', function(req, res) {
-
     Metric.find(req.query, function(err, result){
         if(!err){
             res.send(result);
         }
-        //throw(err);
+        res.send(err);
     });
 });
 
 router.post('/', function(req, res){
-    console.log(req.body);
     var metric = new Metric(req.body);
     metric.save(function(err, result){
         if(!err){
             res.send(result);
         }
-        //res.send(err);
+        res.send(err);
     });
 });
 
